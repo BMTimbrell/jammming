d913d20e8dbd1c5aac85e0dbb4598b94a572529a
Ben <bmtimbrell@gmail.com>
2023-09-20 22:17:27 +0100

    updated log in text

--- src/App.js
+++ src/App.js
@@ -59,7 +59,7 @@ function App() {
     <div className="App">
       <h1>Jammming</h1>
       <div className={window.location.hash ? 'hidden' : ''}>
-        <h2 style={{color: '#DDE6ED'}}>You must login to Spotify to use this app</h2>
+        <h2 style={{color: '#DDE6ED'}}>You must log into Spotify to use this app</h2>
         <button id='login' onClick={() => requestAccessToken()}>Log in</button>
       </div>
       <div className={!window.location.hash ? 'hidden' : 'main'}>
